420cb3fb742542d1ea18b8177bb080ef7aafbbed
Igor Valet <igor_valet@hotmail.com>
2023-04-14 10:10:47 +0000

    Make sources derive Clone

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,18 @@ pub mod dash7;
 pub mod hardware;
 pub mod json;
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct FileSource {
     pub path: String,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct SerialSource {
     pub port: String,
     pub baud_rate: u32,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 #[serde(tag = "type")]
 #[serde(rename_all = "lowercase")]
 pub enum Source {
