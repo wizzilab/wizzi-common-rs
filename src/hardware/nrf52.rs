06516c2d9fdaf06a57944894c5dffa9daa708e4a
Igor Valet <igor_valet@hotmail.com>
2023-04-14 15:33:05 +0000

    [hardware/nrf52] Explicit TI dividers

--- src/hardware/nrf52.rs
+++ src/hardware/nrf52.rs
@@ -1,5 +1,8 @@
-pub const TI_DURATION: std::time::Duration = std::time::Duration::from_micros(1_000_000 / 1024);
-pub const XTI_DURATION: std::time::Duration =
-    std::time::Duration::from_micros(1_000_000 / (16 * 1024));
+pub const TI_DIVIDER: u64 = 1024;
+pub const XTI_PER_TI: u64 = 16;
+pub const XTI_DIVIDER: u64 = XTI_PER_TI * TI_DIVIDER;
 
-pub const XTI_PER_TI: u32 = 16;
+pub const TI_DURATION: std::time::Duration =
+    std::time::Duration::from_micros(1_000_000 / TI_DIVIDER);
+pub const XTI_DURATION: std::time::Duration =
+    std::time::Duration::from_micros(1_000_000 / XTI_DIVIDER);
